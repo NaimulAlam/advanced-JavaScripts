ca67ad8ea002289b425b28d71b24b6e24d8f5744
Naimul Alam <naimul2707@gmail.com>
2021-02-04 22:32:41 +0100

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+
+// const premik = { name: "smart dude", phone: 123123};
+// console.log(premik.gf);
+
+// let fun = undefined; //not recommended
+// console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
