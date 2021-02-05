95a5c5dac3b5fd610ce35ba08011d6f3c8306caf
Naimul Alam <naimul2707@gmail.com>
2021-02-05 03:07:50 +0100

    tried to understand closure, split, splice then break-continue

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if (result > 9){
+        let mood = "happy";
+        mood = "fishy";
+        mood = "funky"
+        console.log(mood);
+    }
+    console.log(day);
+    let day = 'friday';
+    // console.log(mood);
+    // console.log(result);
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(output);
+// console.log(bonus);
